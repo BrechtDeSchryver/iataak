aff18bf22356d6acb0fb93729bba6abb3b0f9d5e
BrechtDeSchryver <brecht.deschryver@student.hogent.Be>
2022-12-05 08:24:11 -0500

    setup exp changes

--- data-workflow/scripts/setupexp.sh
+++ data-workflow/scripts/setupexp.sh
@@ -3,16 +3,18 @@
 set -o errexit
 set -o nounset
 set -o pipefail
+
 SCRIPTDIR="/home/osboxes/Desktop/git/iataak/data-workflow/scripts"
-GITLOC = "/home/osboxes/Desktop/git/iataak/data-workflow"
+GITLOC="/home/osboxes/Desktop/git/iataak/data-workflow"
+
 createcrontab(){
     tempfile="$SCRIPTDIR/tempcron.txt"
     touch "$tempfile"
-    pythonV=$(python -V)
+    pythonV=$(python3 -V | cut -d" " -f2 |  cut -d"." -f1,2)
     {
     printf "*/5 * * * * %s/automated.sh\n" "$SCRIPTDIR"
-    printf "0 */1 * * * /bin/%s %s/analyse.py\n" "$pythonV" "$SCRIPTDIR"
-    printf "0 */1 * * * /bin/%s %s/raportg.py\n" "$pythonV" "$SCRIPTDIR" 
+    printf "0 */1 * * * /bin/python%s %s/analyse.py\n" "$pythonV" "$SCRIPTDIR"
+    printf "0 */1 * * * /bin/python%s %s/raportg.py\n" "$pythonV" "$SCRIPTDIR" 
     } >> "$tempfile"
     printf "new crontab created crontab content:\n" >>$1
     printf "%s" >>$1 
@@ -20,9 +22,11 @@ createcrontab(){
     rm "$tempfile"
 }
 setupenvvars(){
-    
+    #wip   
+    echo "wip"
 }
 pythonlibdownload(){
+    printf "downloading pythong libraries:\n" >> "$logfile"
     pip install lorem >> "$1"
     pip install pandas >> "$1"
     pip install matplotlib >> "$1"
@@ -31,11 +35,14 @@ pythonlibdownload(){
 }
 setupjq(){
     printf "Sudo user password word gevraagd voor het instaleren van de jq package die gebruikt word in dit script\n";
-    sudo apt install jq >> "$1";
+    sudo apt install jq >> "$1" 2> /dev/null;
 }
 init(){
-    logfile="$GITLOC/logs/setuplog";
-    printf "setting up automatic download and upload" > "$logfile";
+    if [[ ! -d "$GITLOC/logs" ]]; then
+        mkdir "$GITLOC/logs";
+    fi
+    logfile="$GITLOC/logs/setuplog.txt";
+    printf "setting up automatic download and upload\n" > "$logfile";
     pythonlibdownload "$logfile";
     createcrontab "$logfile";
     setupjq "$logfile";
