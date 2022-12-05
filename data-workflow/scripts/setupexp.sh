832e3364138ccc757eb46d771d2c01b9bc278d0e
Brecht De Schryver <brecht.deschryver@student.hogent.be>
2022-12-05 18:03:21 +0100

    envcheck added

--- data-workflow/scripts/setupexp.sh
+++ data-workflow/scripts/setupexp.sh
@@ -3,10 +3,10 @@
 set -o errexit
 set -o nounset
 set -o pipefail
-
-SCRIPTDIR="/home/osboxes/Desktop/git/iataak/data-workflow/scripts"
-GITLOC="/home/osboxes/Desktop/git/iataak/data-workflow"
-
+#default waarden mogen aangepast worden
+SCRIPTDIR_default="/home/osboxes/Desktop/git/iataak/data-workflow/scripts"
+GITLOC_default="/home/osboxes/Desktop/git/iataak/data-workflow"
+#maakt een cronetab aan on files te runnen
 createcrontab(){
     tempfile="$SCRIPTDIR/tempcron.txt"
     touch "$tempfile"
@@ -21,10 +21,7 @@ createcrontab(){
     crontab "$tempfile"
     rm "$tempfile"
 }
-setupenvvars(){
-    #wip   
-    echo "wip"
-}
+#download python packages
 pythonlibdownload(){
     printf "downloading pythong libraries:\n" >> "$logfile"
     pip install lorem >> "$1"
@@ -33,11 +30,60 @@ pythonlibdownload(){
     pip install datetime >> "$1"
     pip install tabulate >> "$1"
 }
+#download jq package
 setupjq(){
     printf "Sudo user password word gevraagd voor het instaleren van de jq package die gebruikt word in dit script\n";
     sudo apt install jq >> "$1" 2> /dev/null;
 }
+# $1 = env var
+# $2 = default value
+# $3 = var used in function
+# zet gebruikte var = default value als de env var niet bestaat
+envcheck(){
+    if [[ -z $1]] then 
+        $3=$2
+    else 
+        $3=$1
+    fi
+}
 init(){
+    if [ "$1" == "-h" ]  [ "$1" == "-?" ]  [ "$1" == "--help" ]; then
+        echo "Usage: setup.sh [OPTIONS] "
+        echo "Dit script instaleerd de benodigde software en maakt een crontab aan"
+        echo "voor het ophalen, analyseren en rapporteren van de data"
+        echo "data word opgehaald van data.stad.gent"
+        echo "OPTIONS:"
+        echo "  -h, -?, --help: toont deze melding"
+        echo "  -v: vraagt om directories en gebruikt deze in plaats van de default waarden in het script"
+        echo "  -d: delete variables die gebruikt worden om het script te runnen"
+        echo "      het programma default dan naar de default waarden in het script"
+        exit 0
+    fi
+    while [ $# -gt 0] do
+        case "$1" in
+    -v)
+      # ask user for directory
+      read -p "Geef de directory waar alle scripts zich bevinden: " input
+      export envscriptdir="$input"
+      read -p "Geef de directory waar de git repo zich bevind: " input
+      export envgitdir="$input"
+      ;;
+    -d)
+      # remove env vars
+        unset envscriptdir
+        unset envgitdir
+    *)
+      # do nothing
+      ;;
+  esac
+  shift
+done
+    $SCRIPTDIR="/"
+    envcheck "$envscriptdir" "$SCRIPTDIR_default" "$SCRIPTDIR"
+    echo "$SCRIPTDIR"
+    $GITLOC="/"
+    envcheck "$envgitdir" "$GITLOC_default" "$GITLOC"
+    echo "$GITLOC"
     if [[ ! -d "$GITLOC/logs" ]]; then
         mkdir "$GITLOC/logs";
     fi
