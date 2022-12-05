ba765b553e4ca3bf1018dbc13cc9c0364a2cc149
Brecht De Schryver <brecht.deschryver@student.hogent.be>
2022-12-05 15:26:28 +0100

    some funnies

--- data-workflow/scripts/raportg.py
+++ data-workflow/scripts/raportg.py
@@ -3,6 +3,7 @@ import os
 import lorem
 from git import Repo
 import time
+from raport import aanmakenrapport, gitpush
 #deze file zal de README.md aanpassen zodat een soort van rapport gegenereerd wordt
 GITFILELOC="/home/osboxes/Desktop/git/iataak"
 GITLOC = "/home/osboxes/Desktop/git/iataak/data-workflow"
@@ -10,60 +11,7 @@ GITLOC = "/home/osboxes/Desktop/git/iataak/data-workflow"
 READMELOC = GITLOC + "/README.md"
 IMAGELOC= GITLOC + "/csvimage"
 TABELSLOC= GITLOC + "/tabels"
-def aanmakenrapport():
-    with open(READMELOC, 'w') as f:
-        f.write('## Rapport van de IATAAK\n')
-        f.write('### Auteurs:\n')
-        f.write(' - [Brecht De Schryver]\n')
-        f.write('### Inhoudsopgave:\n')
-        f.write(' - [Inleiding](#inleiding)\n')
-        f.write(' - [Uitvoering](#uitvoering)\n')
-        f.write(' - [Resultaten](#resultaten)\n')
-        f.write(' - [Conclusie](#conclusie)\n')
-        f.write('### Inleiding\n')
-        f.write('In dit rapport wordt de uitvoering van de IATAAK beschreven. voor deze taak maakte ik gebruik van het dataportaal van stad gent. Ik nam een kijkje naar de data van real time beztting van parkeergarages in Gent\n')
-        f.write('### Uitvoering\n')
-        f.write('1.1\n')
-        f.write(' - tijdens dit deel werd de data gescraped\n')
-        f.write('[bash script](https://github.com/BrechtDeSchryver/iataak/blob/main/data-workflow/scripts/webscraper.sh)\n')
-        f.write('\n1.2\n')
-        f.write(' - tijdens dit deel werd de data getransformeerd\n')
-        f.write('[bash script](https://github.com/BrechtDeSchryver/iataak/blob/main/data-workflow/scripts/transform.sh)\n')
-        f.write('\n')
-        f.write(' - dit genereerd een SC2001 warning die niet valt op te lossen omdat de sed code te complex is zie [hier](https://www.shellcheck.net/wiki/SC2001)\n')
-        f.write('\n1.3\n')
-        f.write(' - tijdens dit deel werd de data gevisualiseerd\n')
-        f.write('[python script](https://github.com/BrechtDeSchryver/iataak/blob/main/data-workflow/scripts/analyse.py)\n')
-        f.write('\n1.4\n')
-        f.write(' - tijdens dit deel werd de data mooi opgemaakt\n')
-        f.write('[python script](https://github.com/BrechtDeSchryver/iataak/blob/main/data-workflow/scripts/report.py)\n')
-        f.write('### Resultaten\n')
-        f.write('\n1.5\n')
-        f.write(' - tijdens dit deel werd het gehele process geautomatiseerd\n')
-        f.write('gegeven dat je alle scripts op de juiste plaats hebt staan en in de code deze gelinkt hebt + je toegang hebt tot de github repo kan je dit script uitvoeren\n')
-        f.write('dit vult elke 5 min de csv files aan en elk uur maakt hij nieuwe raporten die hij dan commit op github\n')
-        f.write('[bash script setup](https://github.com/BrechtDeSchryver/iataak/blob/main/data-workflow/scripts/setup.sh)\n')
-        f.write('[bash script automatic run](https://github.com/BrechtDeSchryver/iataak/blob/main/data-workflow/scripts/automated.sh)\n')
-        for file in os.listdir(IMAGELOC):
-            f.write("## grafiek van " + str(file.split(".",2)[0]) + "\n")
-            f.write(lorem.paragraph()+"\n")
-            f.write('![image]('"https://github.com/BrechtDeSchryver/iataak/blob/main/data-workflow/csvimage/"+file+')\n')
-        for file in os.listdir(TABELSLOC):
-            f.write("## tabel van " + str(file.split(".",1)[0]) + "\n")
-            f.write(lorem.paragraph()+"\n")
-            with open(os.path.join(TABELSLOC, file)) as f2:
-                f.write('\n')
-                f.write(f2.read())
-                f.write('\n')
-        f.write('### Conclusie\n')
-        f.write(lorem.paragraph()+"\n")
-        f.write("Goodbye now")
-def gitpush():
-    repo = Repo(GITFILELOC)
-    repo.git.add(update=True)
-    repo.index.commit('automated commit of rapport at time=' + str(time.strftime("%d/%m/%Y")))
-    origin = repo.remote(name='origin')
-    origin.push()
+
 def main():
     aanmakenrapport()
     gitpush()
