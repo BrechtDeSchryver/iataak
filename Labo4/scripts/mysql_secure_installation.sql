da9f462310f8d7eb52c1b0864e681861e172dc5f
BrechtDeSchryver <brecht.deschryver@student.hogent.Be>
2022-12-05 09:38:01 -0500

    fixed

--- Labo4/scripts/mysql_secure_installation.sql
+++ Labo4/scripts/mysql_secure_installation.sql
@@ -3,15 +3,12 @@ DROP USER IF EXISTS www_user;
 
 CREATE DATABASE www_db;
 CREATE TABLE www_db.todo_list (
-  item_id INT AUTO_INCREMENT,
-  content VARCHAR(255),
-  PRIMARY KEY(item_id)
+ item_id INT AUTO_INCREMENT,
+ content VARCHAR(255),
+ PRIMARY KEY(item_id)
 );
 INSERT INTO www_db.todo_list (content)
-VALUES
-  ("Buy milk"),
-  ("Update world domination plans"),
-  ("Call mom");
+VALUES ("Buy milk"),("Update world domination plans"),("Call mom");
 
 GRANT ALL ON www_db.* TO 'www_user'@'localhost' IDENTIFIED BY 'letmein';
 FLUSH PRIVILEGES;
