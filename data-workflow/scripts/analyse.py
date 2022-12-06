5975a831caba8f40338e207574456b6f244aee81
Brecht De Schryver <brecht.deschryver@student.hogent.be>
2022-12-06 02:47:46 +0100

    add

--- data-workflow/scripts/analyse.py
+++ data-workflow/scripts/analyse.py
@@ -55,7 +55,7 @@ def plot_data(df,name):
     ax.xaxis_date()
     plt.xlabel('timestamp')
     plt.ylabel('amount of cars')
-    plt.xticks(x[::288], rotation=90)
+    plt.xticks(range(0, len(x), 288),x[::288])
     plt.plot(x, y, label="occupation")
     plt.plot(x, df['totalcapacity'], label="max_occupation")
     plt.title(name)
