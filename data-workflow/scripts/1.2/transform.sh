2bfac83eb085cef145bb667a98c7ad1bdceb17c5
Brecht De Schryver <brecht.deschryver@student.hogent.be>
2022-12-10 01:50:36 +0100

    changes fix shellcheck

--- data-workflow/scripts/1.2/transform.sh
+++ data-workflow/scripts/1.2/transform.sh
@@ -56,7 +56,7 @@ init(){
         for parking in $parkings
         do  
             #verwijdert de " uit de naam van de parkeergarage
-            newparking=$(echo "$parking" | sed 's/"//g');
+            newparking=${parking//\"/""}
             #controleerd of de csv file van de parkeergarage al bestaat
             if [[ ! -e "$DIRECTORY/csv/$newparking.csv" ]]; then
                 newcsv "$newparking";
