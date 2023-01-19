53dae4c333d160c36dc1804db404851ebaa920d5
Brecht De Schryver <brecht.deschryver@student.hogent.be>
2023-01-19 12:41:05 +0100

    passphrase fix

--- Labo7/scripts/passphrase.sh
+++ Labo7/scripts/passphrase.sh
@@ -1,33 +1,104 @@
-#!/bin/bash
-set -o errexit   # abort on nonzero exitstatus
-set -o nounset   # abort on unbound variable
-set -o pipefail  # don't hide errors within pipes
-# default values
+#! /bin/bash
+## Usage: ./passphrase.sh [N] [WORDLIST]
+##        ./passphrase.sh -h|--help
+##
+## Generate a passphrase, as suggested by http://xkcd.com/936/
+##
+##   N
+##            The number of words the passphrase should consist of
+##            Default: 4
+##   WORDLIST
+##            A text file containing words, one on each line
+##            Default: /usr/share/dict/words
+##
+## OPTIONS
+##   -h, --help
+##              Print this help message
+##
+## EXAMPLES
+##
+##   $ ./passphrase.sh -h
+##   ...
+##   $ ./passphrase.sh
+##   unscandalous syagush merest lockout
+##   $ ./passphrase.sh /usr/share/hunspell/nl_BE.dic 3
+##   tegengif/Ze Boevink/PN smekken 
+
+#---------- Shell options -----------------------------------------
+set -o errexit   
+set -o nounset   
+set -o pipefail  
+
+#---------- Variables ---------------------------------------------
+
 num_words=4
 word_file=/usr/share/dict/words
 
+#---------- Main function -----------------------------------------
+main() {
+  process_cli_args "${@}"
+  generate_passphrase
+}
 
-if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
-  echo "Usage: passphrase.sh [N] [WORDS]"
-  echo "Generates a random passphrase with N words (default: 4)"
-  echo "WORDS is the file containing the list of words to use (default: /usr/share/dict/words)"
-  exit 0
-fi
+#---------- Helper functions --------------------------------------
 
-if [ $# -gt 0 ]; then
-  num_words="$1"
-  if [ $# -gt 1 ]; then
-    word_file="$2"
+# Usage: generate_passphrase
+# Generates a passphrase with ${num_words} words from ${word_list}
+generate_passphrase() {
+  : # TODO
+  passphrase=$(shuf -n "$num_words" "$word_file" | tr '\n' ' ')
+}
+
+# Usage: process_cli_args "${@}"
+#
+# Iterate over command line arguments and process them
+process_cli_args() {
+  : # TODO
+
+  # If the number of arguments is greater than 2: print the usage
+  # message and exit with an error code
+  if [ $# -gt 2 ]; then
+    usage
+    exit 1
   fi
-fi
+# Loop over all arguments
+for arg in "${@}"; do
+    # Use a case statement to determine what to do
+    case "${arg}" in
 
+      # If -h or --help was given, call usage function and exit
+      -h|--help)
+        parse_usage
+        exit 0
+        ;;
 
-if [ ! -f "$word_file" ]; then
-  >&2 echo "Error: file $word_file does not exist"
-  exit 1
-fi
+      # If any other option was given, print an error message and exit with status 1
+      -*)
+        echo "Error: unknown option ${arg}"
+        exit 1
+        ;;
 
+      # In all other cases, we assume N or WORD_LIST was given
+      *)
+        # If the argument is a file, we're setting the word_list variable
+        if [ -f "$arg" ]; then
+          word_list="$arg"
+        else
+          # If not, we assume it's a number and we set the num_words variable
+          num_words="$arg"
+        fi
+        ;;
+    esac
+done
+}
 
-passphrase=$(shuf -n "$num_words" "$word_file" | tr '\n' ' ')
+# Print usage message on stdout by parsing start of script comments.
+# - Search the current script for lines starting with `##`
+# - Remove the comment symbols (`##` or `## `)
+usage() {
+  script_path="$(realpath "$0")"
+  echo"$(sed -n '/^##/s/^## *//p' "$script_path")"
+}
 
-echo "$passphrase"
+# Call the main function
+main "${@}"
