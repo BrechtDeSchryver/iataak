a6d0816845cf820f5ae34b96f0c30f808e4539dd
Brecht De Schryver <brecht.deschryver@student.hogent.be>
2023-01-19 01:21:34 +0100

    last changes voor mondeling

--- Labo4/scripts/ip-info.sh
+++ Labo4/scripts/ip-info.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-# voor elke interface ip adres en netmask
-ip -brief addr | grep -v "lo:" | awk '{print $1 "/" $3}'
+echo -e "Big bingus"
 
-# default gateway
-ip route | grep default | awk '{print "Default gateway: "$3" ("$5")"}'
+echo -e "\033[1;34mInterfaces:\033[0m"
+ip -br addr | awk '{print $3 ": " $4}'
 
-# dns servers
-resolvectl | grep DNS | awk '{print "DNS server: "$NF}'
+
+echo -e "\033[1;34mDefault gateway:\033[0m"
+ip route | awk '/default/ {print $3 " (" $5 ")"}'
+
+echo -e "\033[1;34mDNS servers:\033[0m"
+resolvectl | awk '/DNS Servers: / {print $3}'
--- Labo7/scripts/passphrase.sh
+++ Labo7/scripts/passphrase.sh
@@ -6,7 +6,7 @@ set -o pipefail  # don't hide errors within pipes
 num_words=4
 word_file=/usr/share/dict/words
 
-# check for -h or --help option
+
 if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
   echo "Usage: passphrase.sh [N] [WORDS]"
   echo "Generates a random passphrase with N words (default: 4)"
@@ -14,7 +14,6 @@ if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
   exit 0
 fi
 
-# parse N and WORDS arguments
 if [ $# -gt 0 ]; then
   num_words="$1"
   if [ $# -gt 1 ]; then
@@ -22,14 +21,13 @@ if [ $# -gt 0 ]; then
   fi
 fi
 
-# check if word file exists
+
 if [ ! -f "$word_file" ]; then
   >&2 echo "Error: file $word_file does not exist"
   exit 1
 fi
 
-# generate passphrase
+
 passphrase=$(shuf -n "$num_words" "$word_file" | tr '\n' ' ')
 
-# print passphrase
 echo "$passphrase"
